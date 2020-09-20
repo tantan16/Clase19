01b24d363a9acce4bd598158bccad6293959cdaf
Tania <59518372+tantan16@users.noreply.github.com>
2020-09-20 10:42:29 -0500

    Agrego ahora sí la clase 19

---
+++ Clase19/main.js
@@ -0,0 +1,68 @@
+//PRIMER EJERCICIO 
+//  let cancion1 = prompt(`Primer canción para playlist`)
+// let cancion2 = prompt(`Segunda canción para playlist`)
+// let cancion3 = prompt(`Tercer canción para playlist`)
+// let cancion4 = prompt(`Cuarta canción para playlist`)
+// let cancion5 = prompt(`Quinta canción para playlist`)
+
+
+// const primerCancion = document.getElementById("s1")
+// primerCancion.textContent= cancion1
+
+// const segundaCancion = document.getElementById("s2")
+// segundaCancion.textContent= cancion2
+
+// const terceraCancion = document.getElementById("s3")
+// terceraCancion.textContent = cancion3
+
+// const cuartaCancion = document.getElementById("s4")
+// cuartaCancion.textContent= cancion4
+
+// const quintaCancion = document.getElementById("s5")
+// quintaCancion.textContent= cancion5
+
+
+//SEGUNDO EJERCICIO
+
+// let opcion1= document.getElementById("boton1")
+// let opcion2= document.getElementById("boton2")
+// let opcion3= document.getElementById("boton3")
+// let respuesta = document.getElementById("respuesta")
+
+
+// opcion2.onclick = () =>{
+//     respuesta.textContent = "correcto"
+//     opcion2.classList.add("verde")
+// }
+
+// opcion1.onclick = () =>{
+//     respuesta.textContent = "incorrecto :("
+//      opcion1.classList.add("rojo")
+// }
+
+// opcion3.onclick = () =>{
+//     respuesta.textContent = "incorrecto :("
+//      opcion3.classList.add("rojo")
+// }
+
+
+// EJERCICIO DEL CONTADOR ***MALE ESTE NO ME SALIÓ** NO PUDE CONVERTIR EL VALOR DE LA STRING A NÚMERO 
+const puntoInicial = document.getElementById("numinicial").parse
+const restar1 = document.getElementById("num1")
+const sumar1 = document.getElementById("num2")
+const restar5 = document.getElementById("num3")
+const sumar5 = document.getElementById("num4")
+const restar10 = document.getElementById("num5")
+const sumar10 = document.getElementById("num6")
+let resultado = 
+
+
+restar1.onclick = () => {
+    restar1 = (puntoInicial - 1)
+    puntoInicial.textContent= restar1
+
+    
+}
+
+console.log(restar1)
+
